e63f4efbd23804720e1dbefb63175a6acf65f64e
warkop619 <navigator619@gmail.com>
2019-09-10 16:06:30 +0700

    initial routes

--- application/routes/web.php
+++ application/routes/web.php
@@ -20,3 +20,31 @@ $router->group(['prefix' => 'auth'], function () use ($router) {
     $router->post('/login', 'AuthController@index');
     $router->post('/logout', 'AuthController@logout');
 });
+
+$router->group(['prefix' => 'results', 'middleware' => 'eauth'], function () use ($router) {
+    $router->get('/', 'ResultsController@index');
+});
+
+$router->group(['prefix' => 'manufacturer', 'middleware' => 'eauth'], function () use ($router) {
+    $router->get('/', 'ManufacturerController@index');
+});
+
+$router->group(['prefix' => 'asset_type', 'middleware' => 'eauth'], function () use ($router) {
+    $router->get('/', 'AssetTypeController@index');
+});
+
+$router->group(['prefix' => 'roles', 'middleware' => 'eauth'], function () use ($router) {
+    $router->get('/', 'RolesController@index');
+});
+
+$router->group(['prefix' => 'stations', 'middleware' => 'eauth'], function () use ($router) {
+    $router->get('/', 'StationsController@index');
+});
+
+$router->group(['prefix' => 'seqscheme', 'middleware' => 'eauth'], function () use ($router) {
+    $router->get('/', 'SeqSchemeController@index');
+});
+
+$router->group(['prefix' => 'seqschemegroup', 'middleware' => 'eauth'], function () use ($router) {
+    $router->get('/', 'SeqSchemeGroupController@index');
+});
